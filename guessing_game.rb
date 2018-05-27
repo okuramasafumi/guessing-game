359e56abc0c19e19ebfa717801d85022d005ff56
Okura Masafumi <masafumi.o1988@gmail.com>
2018-05-27 16:19:09 +0900

    Extract some code into its own method

--- guessing_game.rb
+++ guessing_game.rb
@@ -3,12 +3,7 @@ require 'securerandom'
 
 class GuessingGame
   def self.play
-    number = while
-               num = SecureRandom.random_number(10000)
-               if num >= 1000 && num.digits.size == num.digits.uniq.size
-                 break num
-               end
-             end
+    number = random_number
 
     trial_number = 10
     puts "You can try #{trial_number} times."
@@ -21,23 +16,44 @@ class GuessingGame
         next
       end
       if input.to_s == number.to_s
-        puts "You win! #{credit} credits left."
-        exit
+        win!(credit)
       end
-      exact = 0
-      patrial = 0
-      0.upto(3) do |i|
-        n1 = number.to_s[i]
-        n2 = input.to_s[i]
-        if n1 == n2
-          exact += 1
-        elsif number.to_s.include?(n2)
-          patrial += 1
-        end
-      end
-      puts "Exact match: #{exact}, partial match: #{patrial}."
+      calc(number, input)
     end
 
+    lose!(number)
+  end
+
+  def self.random_number
+    while
+      num = SecureRandom.random_number(10000)
+      if num >= 1000 && num.digits.size == num.digits.uniq.size
+        break num
+      end
+    end
+  end
+
+  def self.calc(number, input)
+    exact = 0
+    patrial = 0
+    0.upto(3) do |i|
+      n1 = number.to_s[i]
+      n2 = input.to_s[i]
+      if n1 == n2
+        exact += 1
+      elsif number.to_s.include?(n2)
+        patrial += 1
+      end
+    end
+    puts "Exact match: #{exact}, partial match: #{patrial}."
+  end
+
+  def self.win!(credit)
+    puts "You win! #{credit} credits left."
+    exit
+  end
+
+  def self.lose!(number)
     puts "You lose! The number was: #{number}"
     exit
   end
